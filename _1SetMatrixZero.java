ec85df97cb8f6ff220036c5fce8bdb0763ef0355
Sushant2 <er.sushant.2@gmail.com>
2022-03-02 23:54:33 +0530

    set matrix zeroe update

--- _1SetMatrixZero.java
+++ _1SetMatrixZero.java
@@ -3,7 +3,36 @@ import java.io.*;
 
 public class _1SetMatrixZero {
     public static void setMatZero(int[][] arr) {
-        
+        int rows = arr.length, cols = arr[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (arr[i][j] == 0) {
+                    // make it's row & column to -1
+                    // for that column
+                    for (int k = 0; k < arr.length; k++) {
+                        if (k != i && arr[k][j] != 0)
+                            arr[k][j] = -1;
+                    }
+                    for (int k = 0; k < arr[i].length; k++) {
+                        if (k != j && arr[i][k] != 0)
+                            arr[i][k] = -1;
+                    }
+                }
+            }
+        }
+        // now change all -1 to zeroes
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (arr[i][j] < 0)
+                    arr[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) throws Exception {
