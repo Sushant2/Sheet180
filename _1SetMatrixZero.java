30467b1472d8169d34c041363213d76fbc0550d7
Sushant2 <er.sushant.2@gmail.com>
2022-03-03 01:26:54 +0530

    set matrix zeroes using optimised approach

--- _1SetMatrixZero.java
+++ _1SetMatrixZero.java
@@ -2,7 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class _1SetMatrixZero {
-    public static void setMatZero(int[][] arr) {
+
+    public static void display(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
+
+    // ! Brute Force -
+    // ? Time Comp - O(n*m)*O(n+m)
+    // ? Space Comp - O(1)
+    public static void setMatZero1(int[][] arr) {
         int rows = arr.length, cols = arr[0].length;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -28,13 +40,36 @@ public class _1SetMatrixZero {
                     arr[i][j] = 0;
             }
         }
+        display(arr);
+    }
 
+    // ! Optimised Approach - using 2 dummy arrays - first one equals to row size &
+    // ! second equals to column size
+
+    public static void setMatZero2(int[][] arr) {
+        int rows = arr.length;
+        int cols = arr[0].length;
+        // 2 dummy arrays
+        int[] rowDummy = new int[rows];
+        int[] colDummy = new int[cols];
+        Arrays.fill(rowDummy, -1);
+        Arrays.fill(colDummy, -1);
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++)
-                System.out.print(arr[i][j] + " ");
-            System.out.println();
+            for (int j = 0; j < cols; j++) {
+                if (arr[i][j] == 0) {
+                    rowDummy[i] = 0;
+                    colDummy[j] = 0;
+                }
+            }
         }
-        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (rowDummy[i] == 0 || colDummy[j] == 0) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+        display(arr);
     }
 
     public static void main(String[] args) throws Exception {
@@ -48,6 +83,7 @@ public class _1SetMatrixZero {
                 mat[i][j] = values.charAt(j) - '0';
             }
         }
-        setMatZero(mat);
+        // setMatZero1(mat);
+        setMatZero2(mat);
     }
 }
