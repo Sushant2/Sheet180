01a68808a0cbe2c81d8b2609884004cd34d875d6
Sushant2 <er.sushant.2@gmail.com>
2022-03-06 23:29:33 +0530

    maximum subarray sum optimised-quadratic in O(n^2)

--- _4MaxSubArraySum.java
+++ _4MaxSubArraySum.java
@@ -3,11 +3,11 @@ import java.io.*;
 
 public class _4MaxSubArraySum {
 
-    // ! time comp - O(n^3), space - O(1) (that subArr arraylist will always hold only 2
-    // ! ele(indexes) at any given time)
+    // ! time comp - O(n^3), space - O(1) (that subArr arraylist will always hold
+    // ! only 2ele(indexes) at any given time)
     public static int maxSubArrSumBrute(int[] arr, ArrayList<Integer> subArr) {
         int n = arr.length;
-        int maxSum = 0;
+        int maxSum = Integer.MIN_VALUE;
         // edge case
         if (n == 1)
             return arr[0];
@@ -27,6 +27,28 @@ public class _4MaxSubArraySum {
         return maxSum;
     }
 
+    // !optimised - Quadratic - O(n^2)
+    public static int maxSubArrSumQuadratic(int[] arr, ArrayList<Integer> subArr) {
+        int maxSum = Integer.MIN_VALUE;
+        int n = arr.length;
+        // edge case
+        if (n == 1)
+            return arr[0];
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum > maxSum) {
+                    maxSum = sum;
+                    subArr.clear();
+                    subArr.add(i);
+                    subArr.add(j);
+                }
+            }
+        }
+        return maxSum;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -36,7 +58,8 @@ public class _4MaxSubArraySum {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(values[i]);
         }
-        int ans = maxSubArrSumBrute(arr, subArr);
+        // int ans = maxSubArrSumBrute(arr, subArr);
+        int ans = maxSubArrSumQuadratic(arr, subArr);
         System.out.println("Maximum Sub Array Sum is: " + ans);
         System.out.println("The subarray is: ");
         for (int i = subArr.get(0); i <= subArr.get(1); i++)
