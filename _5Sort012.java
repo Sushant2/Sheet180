e26df7c5058324e51f32458a1390d815e72a2429
Sushant2 <er.sushant.2@gmail.com>
2022-03-09 20:45:53 +0530

    sort 0 1 2 initials

---
+++ _5Sort012.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class _5Sort012 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
