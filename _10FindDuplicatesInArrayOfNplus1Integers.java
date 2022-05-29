fed330cf4a9a03c636f56d124acce52859a8f23b
Sushant2 <er.sushant.2@gmail.com>
2022-05-29 23:44:30 +0530

    optimised code

--- _10FindDuplicatesInArrayOfNplus1Integers.java
+++ _10FindDuplicatesInArrayOfNplus1Integers.java
@@ -1,5 +1,6 @@
 //leetcode - 287
 
+//Brute Force - time - O(nlogn + n) - space - O(1)
 class Solution {
     public int findDuplicate(int[] nums) {
         // brute force - sorting
@@ -8,6 +9,21 @@ class Solution {
             if (nums[i] == nums[i + 1])
                 return nums[i];
 
+        return -1;
+    }
+}
+
+// Optimised Code - time - O(n) - space - O(n)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // little bit optimised using frequency array
+        int[] freq = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            if (freq[nums[i]] != 0)
+                return nums[i];
+            else
+                freq[nums[i]] = 1;
+        }
         return -1;
     }
 }
