e488ad44a02a1fe46d088a9f180eb40c6057956e
Sushant2 <er.sushant.2@gmail.com>
2022-06-03 09:02:46 +0530

    intersection of 2 linkedlist with optimisation using hashing

--- _31IntersectionOf2LL.java
+++ _31IntersectionOf2LL.java
@@ -1,6 +1,6 @@
 //Leetcode - 160 - Intersection of Two Linked Lists
 
-//Brute Force - 2 nested loops - time comp - O(n^2) - space comp - O(1)
+//Brute Force - 2 nested loops - time comp - O(m*n) - space comp - O(1)
 
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
@@ -10,6 +10,24 @@ public class Solution {
                 if (curr == temp)
                     return curr;
             }
+        }
+        return null;
+    }
+}
+
+//Optimised Approach - Using Hashing 
+//time comp - O(m+n) - space comp - O(m)
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode curr = headA;
+        while (curr != null) {
+            set.add(curr);
+            curr = curr.next;
+        }
+        for (ListNode temp = headB; temp != null; temp = temp.next) {
+            if (set.contains(temp))
+                return temp;
         }
         return null;
     }
