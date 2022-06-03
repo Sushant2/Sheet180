044ee567ac195216a71d490cebcc89bfd82ea02d
Sushant2 <er.sushant.2@gmail.com>
2022-06-03 19:27:14 +0530

    return the starting node of cycle using optmisation with slow, fast, entry pointer

--- _35LinkedListCycle2.java
+++ _35LinkedListCycle2.java
@@ -1,4 +1,5 @@
 //leetcode - 142 - Linked List Cycle 2
+//Return the starting node of cycle
 
 //brute force - using hashing
 //time comp - O(n) - space comp - O(n)
@@ -13,6 +14,30 @@ public class Solution {
                 return head;
             set.add(head);
             head = head.next;
+        }
+        return null;
+    }
+}
+
+//Optimisation using fast,slow & entry pointer 
+//time comp - O(n) - space comp - O(1)
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next==null) return null;
+        //using slow & fast pointer & entry pointer for finding starting node of loop  
+        ListNode slow = head, fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                ListNode entry = head;
+                while(entry!=slow){
+                    slow = slow.next;
+                    entry = entry.next;
+                }
+                return slow;
+            }
         }
         return null;
     }
