7a351d953ebe01e66c79cd43c184bfebcc9b7ef0
Sushant2 <er.sushant.2@gmail.com>
2022-06-03 17:58:06 +0530

    palindrome a LL using optimisation

--- _34PalindormeLL.java
+++ _34PalindormeLL.java
@@ -21,3 +21,52 @@ class Solution {
         return true;
     }
 }
+
+// Optimal Approach -
+// steps - find middle, reverse right half, compare first & second half
+// time comp - O(n) - space comp - O(1)
+
+class Solution {
+    private ListNode getMiddle(ListNode head) {
+        ListNode slow = head, fast = head;
+        ListNode prev = null; // to get the mid in case of even no. of nodes
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // in case of even size of LL
+        if (fast == null)
+            return prev;
+        return slow;
+    }
+
+    private ListNode reverse(ListNode node) {
+        // uisng pointer iterative
+        ListNode prev = null;
+        ListNode curr = node;
+        while (curr != null) {
+            ListNode ahead = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = ahead;
+        }
+        return prev;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+        // find middle of LL
+        ListNode mid = getMiddle(head);
+        // to get the right half reversed of LL
+        ListNode second = reverse(mid.next);
+        while (head != null && second != null) {
+            if (head.val != second.val)
+                return false;
+            head = head.next;
+            second = second.next;
+        }
+        return true;
+    }
+}
