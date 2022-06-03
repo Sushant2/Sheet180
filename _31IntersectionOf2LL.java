1c86b7261482fdbb653ac7c9134789eac3f4caf6
Sushant2 <er.sushant.2@gmail.com>
2022-06-03 10:48:08 +0530

    intersection of 2 linked list using most optmisation

--- _31IntersectionOf2LL.java
+++ _31IntersectionOf2LL.java
@@ -80,3 +80,19 @@ public class Solution {
         return null;
     }
 }
+
+//Most Optimised Solution - Another approach - short code
+//time comp - O(2^m) - space - O(1)
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+        ListNode d1 = headA, d2 = headB;
+        while (d1 != d2) {
+            d1 = (d1 == null) ? headB : d1.next;
+            d2 = (d2 == null) ? headA : d2.next;
+        }
+        return d1;
+    }
+}
