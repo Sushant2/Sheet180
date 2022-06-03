a08bff3e54fb3ad348eaa4b27bce174b4bd33bd8
Sushant2 <er.sushant.2@gmail.com>
2022-06-03 11:22:34 +0530

    detect a cycle in linked list optimised approach - fast & slow pointer

--- _32DetectCycleINLL.java
+++ _32DetectCycleINLL.java
@@ -16,3 +16,23 @@ public class Solution {
         return false;
     }
 }
+
+
+//Optimised Approach - using 2 pointers - fast & slow
+//time comp - O(n) - space comp - O(1)
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        // using efficient method - using 2 pointers - fast & slow
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
