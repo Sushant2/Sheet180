3e395c4d5c44f84d650ebd3b8ff6c84d2e54487f
Sushant2 <er.sushant.2@gmail.com>
2022-06-04 09:53:32 +0530

    rotate a linked list using optimisation

--- _37RotateALL.java
+++ _37RotateALL.java
@@ -29,3 +29,31 @@ class Solution {
         return head;
     }
 }
+
+
+//Optimised Code - time comp - O(len) + O(len - (len%k)) - space cpmp - O(1)
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        // optimised approach
+        if (head == null || head.next == null) {
+            return head;
+        }
+        int len = 1;
+        ListNode curr = head;
+        while (curr.next != null) {
+            len++;
+            curr = curr.next;
+        }
+        ListNode newEnd = head;
+        // link last node to first node
+        curr.next = head;
+        // when k is more than length of list
+        k = k % len;
+        for (int i = 1; i < len - k; i++) {
+            newEnd = newEnd.next;
+        }
+        head = newEnd.next;
+        newEnd.next = null;
+        return head;
+    }
+}
