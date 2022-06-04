90300d0bbfd0dd71b0024494972f6067faaddc43
Sushant2 <er.sushant.2@gmail.com>
2022-06-04 08:29:27 +0530

    Rotate a linked list using updated bruteForce

--- _37RotateALL.java
+++ _37RotateALL.java
@@ -1,15 +1,24 @@
 //Leetcode - 61 - Rotate a Linked List
 
-//Brute Force
+//Brute Force - 
+//time comp - O(k*len) - space - O(1)
 
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next==null)
+        if (head == null || head.next == null)
             return head;
-        //brute force
-        for(int i = 1;i<=k;i++){
-            ListNode curr = head, prev = null;;
-            while(curr.next!=null){
+        // brute force
+        int len = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            len++;
+            temp = temp.next;
+        }
+        k = k % len;
+        for (int i = 1; i <= k; i++) {
+            ListNode curr = head, prev = null;
+            ;
+            while (curr.next != null) {
                 prev = curr;
                 curr = curr.next;
             }
@@ -20,4 +29,3 @@ class Solution {
         return head;
     }
 }
-
