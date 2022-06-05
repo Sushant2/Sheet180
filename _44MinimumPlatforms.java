fbd80c55ce1a016ee4d2487f368c75986855f41a
Sushant2 <er.sushant.2@gmail.com>
2022-06-05 16:49:37 +0530

    minimum platforms optimised code using sorting & 2 pointers

--- _44MinimumPlatforms.java
+++ _44MinimumPlatforms.java
@@ -17,3 +17,31 @@ static int findPlatform(int arr[], int dep[], int n)
         return maxPlatform;
     }
     
+
+//Optimised Code - using sorting & 2pointer approach
+// time comp - O(n*logn) - space comp - O(1)
+
+ static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        //sort both arrays
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int maxPlatform = 1, count = 1;
+        //2 pointer approach 
+        int i = 1, j = 0; //i =1, cos we starting from 2nd arrival train, assuming one has already arrived
+        while(i<n && j<n){
+            if(arr[i]<=dep[j]){
+                count++;
+                i++;
+            }else if(arr[i]>dep[j]){
+                count--;
+                j++;
+            }
+            if(count > maxPlatform)
+                maxPlatform = count;
+        }
+        return maxPlatform;
+    }
+    
+}
