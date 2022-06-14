5de578469e7ed216ab1e27f69e057a5070d9474e
Sushant2 <er.sushant.2@gmail.com>
2022-06-14 13:03:35 +0530

    find all permutation of a given array/string using brute force

---
+++ _54Permutations.java
@@ -0,0 +1,31 @@
+//Leetcode - 46 - Permutations
+
+//Brute Force - using extra DS - freq array
+//time comp - O((n!)*n)
+//space comp - O(n) + O(n) for recursion call stack space
+class Solution {
+    private void recurPermute(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean[] freq) {
+        if (ds.size() == nums.length) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!freq[i]) {
+                freq[i] = true;
+                ds.add(nums[i]);
+                recurPermute(nums, ds, ans, freq);
+                ds.remove(ds.size() - 1);
+                freq[i] = false;
+            }
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        // brute force - using extra DS - freq array
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean freq[] = new boolean[nums.length];
+        recurPermute(nums, ds, ans, freq);
+        return ans;
+    }
+}
