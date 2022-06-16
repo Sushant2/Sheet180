b2ed10c976f2b0a9f572ddc1e1311ef22bbd91f5
Sushant2 <er.sushant.2@gmail.com>
2022-06-16 11:29:33 +0530

    nth root of an integer

--- _60NRootOfAnInteger.java
+++ _60NRootOfAnInteger.java
@@ -29,3 +29,34 @@ public class Solution {
     }
 }
 
+// GFG Code - All test cases pass
+// time comp - O(n*logn)
+// space comp - O(1)
+
+class Solution {
+    private long multiply(int number, int n, int m) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans *= number;
+            if (ans > m)
+                break;
+        }
+        return ans;
+    }
+
+    public int NthRoot(int n, int m) {
+        // code here
+        int low = 1;
+        int high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (multiply(mid, n, m) == m)
+                return mid;
+            else if (multiply(mid, n, m) < m)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+}
