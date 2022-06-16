7ca310a4c1b7cccf3406de5bba488be68bc84b4d
Sushant2 <er.sushant.2@gmail.com>
2022-06-16 21:08:19 +0530

    matrix median using optimised approach

--- _61MatrixMedian.java
+++ _61MatrixMedian.java
@@ -25,3 +25,48 @@ public class Solution {
         return list.get(median);
     }
 }
+
+//Optimised code - 
+// monotonically increasing in nature
+// using binary search
+
+//time comp - (log(2^32)*n*log(m))
+//space comp - (1)
+
+import java.util.ArrayList;
+
+public class Solution {
+    private static int numberLessThanEqualToMid(ArrayList<Integer> row, int mid) {
+        int low = 0;
+        int high = row.size() - 1;
+        while (low <= high) {
+            int md = (low + high) >> 1;
+            if (row.get(md) <= mid)
+                low = md + 1;
+            else
+                high = md - 1;
+        }
+        return low;
+    }
+
+    public static int getMedian(ArrayList<ArrayList<Integer>> matrix) {
+        // Write your code here.
+        int row = matrix.size();
+        int col = matrix.get(0).size();
+        // using binary search
+        int low = 1;
+        int high = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            int count = 0;
+            for (int i = 0; i < row; i++) {
+                count += numberLessThanEqualToMid(matrix.get(i), mid);
+            }
+            if (count <= ((row * col) / 2)) {
+                low = mid + 1;
+            } else
+                high = mid - 1;
+        }
+        return low;
+    }
+}
