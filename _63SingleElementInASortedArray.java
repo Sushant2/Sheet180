8c443f56dd2e4f02950213bb4cb46b7cfb64d422
Sushant2 <er.sushant.2@gmail.com>
2022-06-16 23:57:24 +0530

    single element in a sorted array using optimisation

--- _63SingleElementInASortedArray.java
+++ _63SingleElementInASortedArray.java
@@ -13,3 +13,29 @@ class Solution {
         return xor;
     }
 }
+
+
+//Optimised approach - sorted array - binary search
+//time comp - O(logn)
+//space comp - O(1)
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0, high = nums.length - 2;
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            if (nums[mid] == nums[mid ^ 1]) {
+                // it's in the left half
+                low = mid + 1;
+            } else {
+                // means it's in the right half, shrink the right half
+                high = mid - 1;
+            }
+        }
+        return nums[low];
+    }
+}
+
+// to be in the left half
+// 1st instance should be at even index
+// 2nd instance should be at odd index(right after first instance)
