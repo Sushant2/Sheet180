e9d1bc451ea8e5d41a9a88ed36f7b628513c4fcd
Sushant2 <er.sushant.2@gmail.com>
2022-06-25 18:56:59 +0530

    Kth largest element in an array using efficient approach

--- _70kthLargestElementInAnArray.java
+++ _70kthLargestElementInAnArray.java
@@ -1,10 +1,29 @@
 //Leetcode 215 - Kth largest Element In An Array
 
 //Naive Approach - using sorting & return kth element
+//time comp - O(nlogn), space comp - O(1)
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         // brute force
         Arrays.sort(nums);
         return nums[nums.length - k];
+    }
+}
+
+//Little bit Efficient - using MAX Heap - Priority Queue
+//time comp - O(n*logn) + O(k)
+//space comp - O(n)
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        // using priority queue
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int x : nums)
+            pq.add(x);
+        int ans = 0;
+        while (k-- > 0) {
+            ans = pq.remove();
+        }
+        return ans;
     }
 }
