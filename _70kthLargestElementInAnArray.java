774d1c160b7017b03892f3e57d32624716c62846
Sushant2 <er.sushant.2@gmail.com>
2022-06-25 18:59:20 +0530

    Kth largest element in an array using most optimal approach-Quick Select

--- _70kthLargestElementInAnArray.java
+++ _70kthLargestElementInAnArray.java
@@ -27,3 +27,47 @@ class Solution {
         return ans;
     }
 }
+
+//Most Optimal Approach - Using Quick Select(Similar to Quick Sort)
+//time comp - O(n), space comp - O(1)
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        // kth largest is equal to (nums.length-k)th smallest
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    public int quickSelect(int[] nums, int left, int right, int k) {
+        // base case
+        if (left == right)
+            return nums[left];
+
+        // int pivotIdx = right;
+        int pivotIdx = new Random().nextInt(right - left + 1) + left;
+        pivotIdx = partition(nums, left, right, pivotIdx);
+        if (pivotIdx == k)
+            return nums[k];
+        else if (pivotIdx < k)
+            return quickSelect(nums, pivotIdx + 1, right, k);
+        else
+            return quickSelect(nums, left, pivotIdx - 1, k);
+    }
+
+    private int partition(int[] nums, int left, int right, int pivotIdx) {
+        int pivot = nums[pivotIdx];
+        // swap pivot with last index
+        swap(nums, pivotIdx, right);
+        pivotIdx = left;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] <= pivot)
+                swap(nums, i, pivotIdx++);
+        }
+        return pivotIdx - 1;
+    }
+
+    private void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+}
