38f372b4f11049058a2e8ba5441ec758e1cd771d
Sushant2 <er.sushant.2@gmail.com>
2022-06-26 17:29:23 +0530

    merge k sorted list using better optimised approach

--- _73MergeKSortedArrays.java
+++ _73MergeKSortedArrays.java
@@ -37,3 +37,55 @@ public static ArrayList<Integer> mergeKSortedArrays(ArrayList<ArrayList<Integer>
         Collections.sort(ans);
         return ans;
 	}
+
+
+//Optimised approach - using divide & conquer - like we used to merge 2 sorted arrays, same, we used for k sorted arrays
+//time comp - O(n*k)*log(k) - Where ‘K’ is the number of arrays and ‘N’ is the average number of elements in every array.
+//(logk) levels, Also, at each level, the ‘K’ arrays are merged
+//space comp - O(n*k)*log(k)
+
+public static ArrayList<Integer> merge(ArrayList<Integer> first, ArrayList<Integer> second){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int m = first.size(), n = second.size();
+        int i = 0, j = 0;
+        while(i<m && j<n){
+            if(first.get(i) < second.get(j)){
+                ans.add(first.get(i));
+                i++;
+            }
+            else{
+                ans.add(second.get(j));
+                j++;
+            }
+        }
+        while(i<m){
+            ans.add(first.get(i));
+            i++;
+        }
+        while(j<n){
+            ans.add(second.get(j));
+            j++;
+        }
+        return ans;
+    }
+    public static ArrayList<Integer> mergeKSortedList(ArrayList<ArrayList<Integer>> kArrays, int low, int high){
+        //base case - only one list, directly return it
+        if(low==high)
+            return kArrays.get(low);
+        //if only 2 lists are there, merge them
+        if(low+1 == high)
+            return merge(kArrays.get(low), kArrays.get(high));
+       
+        int mid = (low+high)>>1;
+        ArrayList<Integer> first = mergeKSortedList(kArrays, low, mid);
+        ArrayList<Integer> second = mergeKSortedList(kArrays, mid+1, high);
+        
+        return merge(first, second);
+       
+    }
+	public static ArrayList<Integer> mergeKSortedArrays(ArrayList<ArrayList<Integer>> kArrays, int k)
+	{
+	    //using divide n conquer
+        ArrayList<Integer> ans = mergeKSortedList(kArrays, 0, k-1);
+        return ans;
+    }
