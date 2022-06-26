dc4a26a2235b117dc4526ad5202c8f0f86c49260
Sushant2 <er.sushant.2@gmail.com>
2022-06-26 17:09:26 +0530

    merge k sorted list using brute force

--- _73MergeKSortedArrays.java
+++ _73MergeKSortedArrays.java
@@ -2,8 +2,12 @@
 
 
 //2 brute force codes - 
+//time comp - O(n*k)*log(n*k) - where k is no of lists & n is average no of elements in every list
+//space comp - O(n*k)
+
 //first - run nested loop, store all ele in min heap & later remove top from min heap one by one & store it in final array ans & return it
 //second - run nested loop, store all elements in arraylsit & return the final array list after sorting
+//first
 public static ArrayList<Integer> mergeKSortedArrays(ArrayList<ArrayList<Integer>> kArrays, int k)
 	{
 		// Write your code here.
@@ -20,4 +24,16 @@ public static ArrayList<Integer> mergeKSortedArrays(ArrayList<ArrayList<Integer>
         return ans;
 	}
 
-    
+    //second
+    public static ArrayList<Integer> mergeKSortedArrays(ArrayList<ArrayList<Integer>> kArrays, int k)
+	{
+		// Write your code here.
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0;i<k;i++){
+            for(int j = 0;j<kArrays.get(i).size();j++){
+                ans.add(kArrays.get(i).get(j));
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+	}
