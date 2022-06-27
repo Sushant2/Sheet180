f2982e44e0c908e0f50d0a84834f74b8443be5fd
Sushant2 <er.sushant.2@gmail.com>
2022-06-27 19:37:23 +0530

    implemented stack using 2 queues - pop efficient

--- _77ImplementStackUsingSingleQueue.java
+++ _77ImplementStackUsingSingleQueue.java
@@ -36,3 +36,41 @@ class MyStack {
         return q1.isEmpty();
     }
 }
+
+// using 2 queues - push in O(n), pop in O(n)
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+    int top;
+
+    public MyStack() {
+        q1 = new ArrayDeque<>();
+        q2 = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        q2.add(x);
+        top = x;
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+        int val = q1.remove();
+        if (!q1.isEmpty())
+            top = q1.peek();
+        return val;
+    }
+
+    public int top() {
+        return top;
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
