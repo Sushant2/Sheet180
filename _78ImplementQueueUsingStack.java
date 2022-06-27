cdd130dc5424c65ee4a3388a40a9927fcf0c0316
Sushant2 <er.sushant.2@gmail.com>
2022-06-27 23:06:08 +0530

    implement queue using stack push/pop efficient - amortized O(1)

--- _78ImplementQueueUsingStack.java
+++ _78ImplementQueueUsingStack.java
@@ -33,3 +33,43 @@ class MyQueue {
         return stk1.isEmpty();
     }
 }
+
+
+// time comp - O(1) amortized
+class MyQueue {
+    Stack<Integer> input;
+    Stack<Integer> output;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        if (output.isEmpty() == false) {
+            return output.pop();
+        }
+        while (!input.isEmpty()) {
+            output.push(input.pop());
+        }
+        return output.pop();
+    }
+
+    public int peek() {
+        if (output.isEmpty() == false) {
+            return output.peek();
+        }
+        while (!input.isEmpty()) {
+            output.push(input.pop());
+        }
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
