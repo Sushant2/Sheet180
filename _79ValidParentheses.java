e760abd618760b32946581de987d2483b9ff7947
Sushant2 <er.sushant.2@gmail.com>
2022-06-28 00:26:41 +0530

    valid Parentheses using better approach

--- _79ValidParentheses.java
+++ _79ValidParentheses.java
@@ -1,6 +1,9 @@
 //Leetcode - 20 - Valid Parentheses
 
 //first approach  - using stack normally
+//time comp - O(n) for string traversal
+//space comp - O(n)
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stk = new Stack<>();
@@ -19,5 +22,49 @@ class Solution {
             }
         }
         return stk.isEmpty();
+    }
+}
+
+//smart approach - using deque & switch or we can solve it with the same approach using stack too
+
+class Solution {
+    public boolean isValid(String s) {
+        // using deque & switch
+        Deque<Character> stack = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    if (stack.isEmpty() || c != stack.pop())
+                        return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+class Solution {
+    public boolean isValid(String s) {
+        // using stack & same previous technique
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || c != stack.pop())
+                return false;
+        }
+        return stack.isEmpty();
     }
 }
