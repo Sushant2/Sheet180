d05ca25e800448f0ab46fadb48ecaf03785b399a
Sushant2 <er.sushant.2@gmail.com>
2022-06-28 13:22:37 +0530

    next greater element to right using optimised approach

--- _80NextGreaterElement1.java
+++ _80NextGreaterElement1.java
@@ -5,18 +5,40 @@
 //space comp - O(1) or output space - O(n) for nge array
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        //brute force
+        // brute force
         int n = nums1.length;
         int m = nums2.length;
         int[] nge = new int[n];
-        for(int i = 0;i<n;i++){
-            int greaterEle = -1, j = m-1;
-            while(j >= 0 && nums1[i] != nums2[j]){
-                if(nums2[j] > nums1[i])
+        for (int i = 0; i < n; i++) {
+            int greaterEle = -1, j = m - 1;
+            while (j >= 0 && nums1[i] != nums2[j]) {
+                if (nums2[j] > nums1[i])
                     greaterEle = nums2[j];
                 j--;
             }
             nge[i] = greaterEle;
+        }
+        return nge;
+    }
+}
+
+// Optimised Approach - using hashmap & stack
+// time comp - O(n)
+// space comp - O(n)
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // using hashmap & stack
+        int[] nge = new int[nums1.length];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stk = new Stack<>();
+        for (int num : nums2) {
+            while (!stk.isEmpty() && num > stk.peek())
+                map.put(stk.pop(), num);
+            stk.push(num);
+        }
+        int i = 0;
+        for (int num : nums1) {
+            nge[i++] = map.getOrDefault(num, -1);
         }
         return nge;
     }
