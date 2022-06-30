e5138296dc7fdf7fa226b29f6aeda275c8026876
Sushant2 <er.sushant.2@gmail.com>
2022-06-30 21:08:35 +0530

    largest rectangle in hisotgram most optimised approach

--- _85LargestRectangleInHistogram.java
+++ _85LargestRectangleInHistogram.java
@@ -74,3 +74,31 @@ class Solution {
         return maxArea;
     }
 }
+
+//Most optimised approach - in one pass
+//time comp - O(n)
+//space comp - O(n)
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stk = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+        // <= cos we'll do it for more more time for heights.length
+        for (int i = 0; i <= n; i++) {
+            while (!stk.isEmpty() && (i == n || heights[stk.peek()] >= heights[i])) {
+                int height = heights[stk.peek()];
+                stk.pop();
+                int width;
+                if (stk.isEmpty() == true)
+                    width = i; // 0 to i
+                else
+                    width = i - stk.peek() - 1;
+
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stk.push(i);
+        }
+        return maxArea;
+    }
+}
