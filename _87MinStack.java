ebc08a12267136cd928ae5f1bd6392a4b62faf65
Sushant2 <er.sushant.2@gmail.com>
2022-07-01 19:41:53 +0530

    min stack using first approach

---
+++ _87MinStack.java
@@ -0,0 +1,40 @@
+//leetcode - 155 Min Stack
+//Put, Pop, getMin, Peek all in O(1)
+
+//Approach - 1
+//using 2 Stacks - if push - push on both, & if pop - pop from both
+
+class MinStack {
+    Stack<Integer> stk;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stk = new Stack<>();
+        minStack = new Stack<>();
+        // initially push INT.MAX in minstack
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        // push in both
+        stk.push(val);
+        minStack.push(Math.min(minStack.peek(), val));
+    }
+
+    public void pop() {
+        stk.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        if (stk.size() == 0)
+            return -1;
+        return stk.peek();
+    }
+
+    public int getMin() {
+        if (stk.size() == 0)
+            return -1;
+        return minStack.peek();
+    }
+}
