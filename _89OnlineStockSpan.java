82a5f6e43634064b1dc49d2dea49e92503e05497
Sushant2 <er.sushant.2@gmail.com>
2022-07-02 16:49:17 +0530

    online stock span slight variation of NGE to left

--- _89OnlineStockSpan.java
+++ _89OnlineStockSpan.java
@@ -9,6 +9,9 @@ So 2 * N times stack operations and N times calls.
 I'll say time complexity is amortized O(1)
 */
 
+// So even if array is [5,4,3,2,1,6]
+// We only compare with top, so its one call for each and answer would be [1,1,1,1,1,6] , but for checking the span for price 6 would require individually checking all the previously added prices on the stack which is O(n). 
+
 //time comp - O(n) - n is no. of prices
 //space comp - O(n)
 
