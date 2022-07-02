c86a308a495f4e550990ddc371d513a8d0a313bd
Sushant2 <er.sushant.2@gmail.com>
2022-07-02 12:30:02 +0530

    minimum stack using another approach w/o stack

--- _87MinStack.java
+++ _87MinStack.java
@@ -91,3 +91,44 @@ class MinStack {
         return mini.intValue();
     }
 }
+
+//Another approach - without using stack
+//LinkedList Nodes
+
+class MinStack {
+    public class Node {
+        int val, min;
+        Node next;
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if (head == null)
+            head = new Node(val, val, null);
+        else
+            head = new Node(val, Math.min(val, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+}
