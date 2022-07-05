c82a356b44c02b42b2897a430ea368c35a07906e
Sushant2 <er.sushant.2@gmail.com>
2022-07-05 23:55:51 +0530

    reverse words in a string using brute force

---
+++ _92ReverseWordsInAString.java
@@ -0,0 +1,32 @@
+//Leetcode - 
+
+//Brute force 
+//time comp - O(n)
+//space comp - O(n)
+
+class Solution {
+    public String reverseWords(String s) {
+        char[] sCharArray = s.toCharArray();
+        Stack<String> stk = new Stack<>();
+        StringBuilder currWord = new StringBuilder();
+        
+        for(char ch : sCharArray){
+            if(ch ==' '){
+                if(currWord.length() > 0)
+                    stk.push(currWord.toString());
+                currWord.setLength(0);
+            }
+            else
+                currWord.append(Character.toString(ch));
+        }
+        
+        if(currWord.length() > 0)
+            stk.push(currWord.toString());
+        currWord.setLength(0);
+        
+        while(!stk.isEmpty()){
+            currWord.append(stk.pop()).append(" ");
+        }
+        return currWord.toString().trim();
+    }
+}
