6b68fb37cc12f0dc9bed5e2667a46444bd6b353e
Sushant2 <er.sushant.2@gmail.com>
2022-07-06 10:57:20 +0530

    reverse words in a string

--- _92ReverseWordsInAString.java
+++ _92ReverseWordsInAString.java
@@ -42,3 +42,50 @@ class Solution {
         return String.join(" ", words);
     }
 }
+
+
+//Another way - without split/trim/stringBuilder 
+class Solution {
+    public String reverseWords(String s) {
+        // without trim/stringBuilder/split
+        int len = s.length();
+        //step 1 : conver it into charArray
+        char[] sCharArr = s.toCharArray();
+        //step 2 : reverse the string
+        reverse(sCharArr, 0, len-1);
+        //step 3 : reverse each word
+        reverseWord(sCharArr, len);
+        //remove leading/trailing/in betweeen spaces
+        return removeSpaces(sCharArr, len);
+    }
+    private void reverse(char[] arr, int l, int r){
+        while(l<r){
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
+    private void reverseWord(char[] arr, int len){
+        int i = 0, j =0;
+        while(i<len){
+        //skip spaces 
+        while(i<j || i < len && arr[i] == ' ') i++;
+        //skip non space
+        while(j<i || j< len && arr[j] != ' ') j++;
+        //reverse the word
+        reverse(arr, i, j-1);
+        }
+    }
+    private String removeSpaces(char[] arr, int len){
+        int i = 0, j = 0;
+        while (j < len) {
+          while (j < len && arr[j] == ' ') j++;             // skip spaces
+          while (j < len && arr[j] != ' ') arr[i++] = arr[j++]; // keep non spaces
+          while (j < len && arr[j] == ' ') j++;             // skip spaces
+          if (j < len) arr[i++] = ' ';                      // keep only one space
+        }
+
+        return new String(arr).substring(0, i);
+        
+    }
+}
