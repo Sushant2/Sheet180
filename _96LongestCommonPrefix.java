b6f720040d6b40297f972e049ae06d8f84c73038
Sushant2 <er.sushant.2@gmail.com>
2022-07-07 23:24:35 +0530

    longest common prefix using divide & conquer

--- _96LongestCommonPrefix.java
+++ _96LongestCommonPrefix.java
@@ -49,3 +49,37 @@ class Solution {
         return strs[0];
     }
 }
+
+
+//Approach 3 - Using Divide & conquer
+//time comp - O(m*n) - where m equal strings with length n in worstcase
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // using divide & conquer
+        // edge case
+        if (strs.length == 0 || strs == null)
+            return "";
+        return LCP(strs, 0, strs.length - 1);
+    }
+
+    private String LCP(String[] strs, int l, int r) {
+        if (l == r)
+            return strs[l];
+        else {
+            int mid = (l + r) >> 1;
+            String lcpLeft = LCP(strs, l, mid);
+            String lcpRight = LCP(strs, mid + 1, r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    String commonPrefix(String left, String right) {
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            if (left.charAt(i) != right.charAt(i))
+                return left.substring(0, i);
+        }
+        return left.substring(0, min);
+    }
+}
