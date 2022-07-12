baa01053ba1855c5a9261d70764ef71b0db1275e
Sushant2 <er.sushant.2@gmail.com>
2022-07-12 08:28:03 +0530

    compare version number without split function

--- _102CompareVersionNumbers.java
+++ _102CompareVersionNumbers.java
@@ -1,6 +1,8 @@
 //Leetcode - 165 - Compare Version Numbers
 
 //Solution 1 - Using split function
+//time comp - O(n)
+//space comp - O(n)
 
 class Solution {
     public int compareVersion(String version1, String version2) {
@@ -17,6 +19,39 @@ class Solution {
                 return -1;
             else if(one > two)
                 return 1;
+        }
+        return 0;
+    }
+}
+
+
+//Solution 2 - without split function
+//time comp - O(n)
+//space comp - O(1)
+
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        // without split function
+        int temp1 = 0, temp2 = 0;
+        int len1 = version1.length(), len2 = version2.length();
+
+        int i = 0, j = 0;
+        while (i < len1 || j < len2) {
+            temp1 = 0;
+            temp2 = 0;
+            while (i < len1 && version1.charAt(i) != '.')
+                temp1 = temp1 * 10 + version1.charAt(i++) - '0';
+            while (j < len2 && version2.charAt(j) != '.')
+                temp2 = temp2 * 10 + version2.charAt(j++) - '0';
+
+            if (temp1 < temp2)
+                return -1;
+            else if (temp1 > temp2)
+                return 1;
+            else {
+                i++;
+                j++;
+            }
         }
         return 0;
     }
