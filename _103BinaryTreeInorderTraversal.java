da71e630cef5149844bd5deb50356a9cb3e8a5a5
Sushant2 <er.sushant.2@gmail.com>
2022-07-12 19:35:31 +0530

    binary tree inorder traversal using stack

--- _103BinaryTreeInorderTraversal.java
+++ _103BinaryTreeInorderTraversal.java
@@ -22,3 +22,32 @@ class Solution {
         util(root.right, ans);
     }
 }
+
+//Using stack
+//time comp - O(n)
+//space comp - O(n)
+
+class Solution {
+    private void pushAllLeft(TreeNode root, Stack<TreeNode> stk) {
+        while (root != null) {
+            stk.push(root);
+            root = root.left;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+        Stack<TreeNode> stk = new Stack<>();
+
+        // push all left
+        pushAllLeft(root, stk);
+        while (!stk.isEmpty()) {
+            TreeNode curr = stk.pop();
+            ans.add(curr.val);
+            pushAllLeft(curr.right, stk);
+        }
+        return ans;
+    }
+}
