90303ad30f8fdc7f3d93b096178ce3cbfca2b58c
Sushant2 <er.sushant.2@gmail.com>
2022-07-12 21:19:13 +0530

    binary tree postorder using 2 stacks

--- _105BinaryTreePostorderTraversal.java
+++ _105BinaryTreePostorderTraversal.java
@@ -20,3 +20,32 @@ class Solution {
         return ans;
     }
 }
+
+//Using 2 stacks
+//time comp - O(n)
+//space comp - O(n)
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        // using 2 stacks
+        List<Integer> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+        Stack<TreeNode> stk1 = new Stack<>();
+        Stack<TreeNode> stk2 = new Stack<>();
+
+        stk1.push(root);
+        while (!stk1.isEmpty()) {
+            TreeNode node = stk1.pop();
+            stk2.push(node);
+            if (node.left != null)
+                stk1.push(node.left);
+            if (node.right != null)
+                stk1.push(node.right);
+        }
+        while (!stk2.isEmpty()) {
+            ans.add(stk2.pop().val);
+        }
+        return ans;
+    }
+}
