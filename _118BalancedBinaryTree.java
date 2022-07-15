426ae46eb1150acbd0ab235680f5d787836ce23d
Sushant2 <er.sushant.2@gmail.com>
2022-07-15 23:50:02 +0530

    balanced binary tree using optimised approach

--- _118BalancedBinaryTree.java
+++ _118BalancedBinaryTree.java
@@ -34,3 +34,30 @@ class Solution {
 
     }
 }
+
+
+//Optimised Approach
+//time comp - O(n)
+//space comp - O(n)
+
+class Solution {
+    private int dfsHeight(TreeNode root) {
+        if (root == null)
+            return 0;
+        int leftHeight = dfsHeight(root.left);
+        if (leftHeight == -1)
+            return -1;
+        int rightHeight = dfsHeight(root.right);
+        if (rightHeight == -1)
+            return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1)
+            return -1;
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight(root) != -1;
+    }
+}
