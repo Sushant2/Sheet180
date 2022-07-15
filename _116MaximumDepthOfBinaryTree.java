07dead63aa0d7708ede228bff72ca9a04fcd8ee8
Sushant2 <er.sushant.2@gmail.com>
2022-07-15 11:29:20 +0530

    maximum depth of binary tree

--- _116MaximumDepthOfBinaryTree.java
+++ _116MaximumDepthOfBinaryTree.java
@@ -16,3 +16,30 @@ class Solution {
         return 1 + Math.max(left, right);
     }
 }
+
+//Using BFS 
+//time comp - O(n)
+//space comp - O(n)
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // using BFS
+        if (root == null)
+            return 0;
+        int height = 0;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            height++;
+            while (size-- > 0) {
+                TreeNode node = q.remove();
+                if (node.left != null)
+                    q.add(node.left);
+                if (node.right != null)
+                    q.add(node.right);
+            }
+        }
+        return height;
+    }
+}
