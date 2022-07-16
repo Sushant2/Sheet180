7018f6f319efe98b9ab05fcd98661ca1fe0594a5
Sushant2 <er.sushant.2@gmail.com>
2022-07-16 22:51:04 +0530

    binary tree zigzag order traversal

--- _121BinaryTreeZigZagOrderTraversal.java
+++ _121BinaryTreeZigZagOrderTraversal.java
@@ -38,3 +38,49 @@ class Solution {
         return ans;
     }
 }
+
+//using 2 stacks
+//time comp - O(n)
+//space comp - O(n)
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        //using 2 stacks
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null)
+            return ans;
+        
+        Stack<TreeNode> ms = new Stack<>();
+        Stack<TreeNode> cs = new Stack<>();
+        ms.push(root);
+        int level = 1;
+        List<Integer> list = new ArrayList<>();
+        while(!ms.isEmpty()){
+            TreeNode curr = ms.pop();
+            list.add(curr.val);
+            //odd levels 1,3,5...ke lie uske childrens(levels 2,4,6...) ko right to left add krna hai, to stack mein left to right push krenge!
+            if(level%2 == 1){
+                if(curr.left!=null)
+                    cs.push(curr.left);
+                if(curr.right!=null)
+                    cs.push(curr.right);
+            }else{
+
+                if(curr.right!=null)
+                    cs.push(curr.right);
+                if(curr.left!=null)
+                    cs.push(curr.left);
+            }
+            
+            if(ms.size() == 0){
+                ms = cs;
+                ans.add(list);
+                cs = new Stack<>();
+                list = new ArrayList<>();
+                level++;
+                
+            }
+        }
+        return ans;
+    }
+}
