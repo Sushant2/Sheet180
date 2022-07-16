a546bdb2443b84b1532cfb08dc5142ebff17d149
Sushant2 <er.sushant.2@gmail.com>
2022-07-16 17:05:58 +0530

    Same tree

--- _120SameTree.java
+++ _120SameTree.java
@@ -14,3 +14,31 @@ class Solution {
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
+
+//Using 1 Queue
+//time comp - O(n)
+//space comp - O(n)
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(p);
+        que.add(q);
+
+        while (!que.isEmpty()) {
+            TreeNode node1 = que.remove();
+            TreeNode node2 = que.remove();
+
+            if (node1 == null && node2 == null)
+                continue;
+            else if (node1 == null || node2 == null || node1.val != node2.val)
+                return false;
+
+            que.add(node1.left);
+            que.add(node2.left);
+            que.add(node1.right);
+            que.add(node2.right);
+        }
+        return true;
+    }
+}
