27b308df4714d237df299a2412791a426a6eff0d
Sushant2 <er.sushant.2@gmail.com>
2022-07-18 20:49:51 +0530

    invert a bianry tree

--- _127MirrororInvertABinaryTree.java
+++ _127MirrororInvertABinaryTree.java
@@ -45,3 +45,28 @@ class Solution {
         return root;
     }
 }
+
+//using BFS
+//time comp - O(n)
+//space comp - O(n)
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.remove();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+
+            if (node.left != null)
+                q.add(node.left);
+            if (node.right != null)
+                q.add(node.right);
+        }
+        return root;
+    }
+}
