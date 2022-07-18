d02e8415dc2308d9c9947017c9e16a48d139f4e8
Sushant2 <er.sushant.2@gmail.com>
2022-07-18 20:24:34 +0530

    mirror tree

---
+++ _127MirrororInvertABinaryTree.java
@@ -0,0 +1,21 @@
+//GFG - mirror tree
+//Leetcode - 226 - Invert a binary tree
+
+//GFG - 
+//time comp - O(n)
+//space comp - O(1)
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+        if (node == null)
+            return;
+        mirror(node.left);
+        mirror(node.right);
+        // swap
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+}
