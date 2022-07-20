49ddb4a89b3a2724c6e7cdb8275e1a901fb0bc75
Sushant2 <er.sushant.2@gmail.com>
2022-07-20 15:11:30 +0530

    construct BST from preorder traversal using naive approach

--- _132ConstructBSTfromPreorderTraversal.java
+++ _132ConstructBSTfromPreorderTraversal.java
@@ -1,6 +1,36 @@
 //leetcode - 1008 - Construct Binary Search Tree from Preorder Traversal
 
-//naive/poor approach
+//naive/brute force
+//time comp - O(n^2)
+//space comp - O(1)
+
+class Solution {
+    private TreeNode helper(int[] preorder, int start, int end) {
+        if (start > end)
+            return null;
+
+        TreeNode root = new TreeNode(preorder[start]);
+
+        // find the index first node of right child
+        int i;
+        for (i = start; i <= end; i++) {
+            if (preorder[i] > root.val)
+                break;
+        }
+
+        root.left = helper(preorder, start + 1, i - 1);
+        root.right = helper(preorder, i, end);
+
+        return root;
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return helper(preorder, 0, preorder.length - 1);
+    }
+}
+
+
+//better approach
 //time comp - O(n*logn)
 //space comp - O(n) + O(n) ~ O(n)
 
