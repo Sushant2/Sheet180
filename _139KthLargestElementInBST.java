a828e5d9b5a59b73fbd5a46e78d8a69913c1c2a7
Sushant2 <er.sushant.2@gmail.com>
2022-07-22 10:43:00 +0530

    Kth largest element in BST using approach

--- _139KthLargestElementInBST.java
+++ _139KthLargestElementInBST.java
@@ -31,3 +31,44 @@ class Solution
         return ans;
     }
 }
+
+//Using Reverse Morris Inorder Traversal
+
+//time comp - O(h)
+//space comp - O(1)
+
+class Solution {
+    // return the Kth largest element in the given BST rooted at 'root'
+    public int kthLargest(Node root, int k) {
+        // Your code here
+        // using morris traversal
+
+        Node curr = root;
+        int ans = 0;
+
+        while (curr != null) {
+            if (curr.right == null) {
+                k--;
+                if (k == 0)
+                    ans = curr.data;
+                curr = curr.left;
+            } else {
+                Node ios = curr.right;
+                while (ios.left != null && ios.left != curr) {
+                    ios = ios.left;
+                }
+                if (ios.left == null) {
+                    ios.left = curr;
+                    curr = curr.right;
+                } else {
+                    ios.left = null;
+                    k--;
+                    if (k == 0)
+                        ans = curr.data;
+                    curr = curr.left;
+                }
+            }
+        }
+        return ans;
+    }
+}
