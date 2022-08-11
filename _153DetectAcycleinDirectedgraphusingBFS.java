962cc06041ee9b57de80f7af4a0e7c14fd8b2be1
Sushant2 <er.sushant.2@gmail.com>
2022-08-11 17:14:37 +0530

    detect a cycle in directed graph using BFS

--- _153DetectAcycleinDirectedgraphusingBFS.java
+++ _153DetectAcycleinDirectedgraphusingBFS.java
@@ -1,3 +1,39 @@
-public class _153DetectAcycleinDirectedgraphusingBFS {
-    
+//GFG - detect a cycle in directed graph
+
+//using BFS - kahn's algo
+
+//time comp - O(n + e) : n for nodes + e for edges
+//space comp - O(n+e) : O(n) + O(n)
+//adjacency list + queue + indegree array
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        //using BFS - kahn's algo
+        int[] indegree = new int[V];
+        for(int i = 0;i<V;i++){
+            for(int x : adj.get(i))
+                indegree[x]++;
+        }
+        
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for(int i = 0;i<V;i++)
+            if(indegree[i] == 0)
+                q.add(i);
+        int countV = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            countV++;
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0)
+                    q.add(it);
+            }
+        }
+        
+        if(countV == V)
+            return false;
+        return true;
+    }
 }
