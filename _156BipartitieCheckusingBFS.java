9b4fa72106be2d61940828268b40080ab086fdfc
Sushant2 <er.sushant.2@gmail.com>
2022-08-11 08:15:09 +0530

    bipartite graph using BFS

--- _156BipartitieCheckusingBFS.java
+++ _156BipartitieCheckusingBFS.java
@@ -1,3 +1,35 @@
-public class _156BipartitieCheckusingBFS {
-    
+//leetcode - is bipartite graph?
+
+class Solution {
+    private boolean checkBipartite(int node, int[] color, int[][] graph){
+        Queue<Integer> q = new ArrayDeque<>();
+        color[node] = 1;
+        q.add(node);
+        while(!q.isEmpty()){
+            int currNode = q.poll();
+            for(int x : graph[currNode]){
+                if(color[x] == -1){
+                    color[x] = 1-color[currNode];
+                    q.add(x);
+                }else if(color[x] == color[currNode])
+                    return false;
+            }
+        }
+        return true;
+    }
+    public boolean isBipartite(int[][] graph) {
+        //the graph is disconnected with multiple component
+        //using BFS
+        int V = graph.length;
+        int[] color = new int[V];
+        Arrays.fill(color, -1);
+        
+        for(int i = 0;i<V;i++){
+            if(color[i]==-1){
+                if(!checkBipartite(i, color, graph))
+                    return false;
+            }
+        }
+        return true;
+    }
 }
