b6c3ae5c8c1d94d628be7225acd18ab2f4087063
Sushant2 <er.sushant.2@gmail.com>
2022-08-13 12:16:09 +0530

    clone a graph using BFS

--- _147CloneAGraph.java
+++ _147CloneAGraph.java
@@ -3,6 +3,7 @@
 //using DFS
 //time comp - O(E.V)
 //E - for all edges, C - for all vertices
+//space comp - O(V) - recursions tack space
 // (fully connected graph)
 
 class Solution {
@@ -24,6 +25,37 @@ class Solution {
         Node[] vis = new Node[101]; //bydefault initialsed with null values
         Node copyN = new Node(node.val);
         dfsTrav(node, copyN, vis);
+        return copyN;
+    }
+}
+
+
+//using BFS
+//time comp - O(E.V)
+//E - for all edges, C - for all vertices
+//space comp - O(V) - queue + O(101) - constant - visited array
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null)
+            return null;
+        Node[] vis = new Node[101]; //bydefault initialsed with null values
+        Deque<Node> q = new ArrayDeque<>();
+        Node copyN = new Node(node.val);
+        vis[copyN.val] = copyN;
+        q.add(node);
+        while(!q.isEmpty()){
+            Node currN = q.poll();
+            for(Node n : currN.neighbors){
+                if(vis[n.val] == null){
+                    Node newNode = new Node(n.val);
+                    vis[n.val] = newNode;
+                    q.add(n);
+                }
+                vis[currN.val].neighbors.add(vis[n.val]);
+            }
+        }
+        
         return copyN;
     }
 }
