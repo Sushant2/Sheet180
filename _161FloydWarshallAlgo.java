2d6f3a9fe2187e98b174370507138fd8473e8ba7
Sushant2 <er.sushant.2@gmail.com>
2022-09-08 18:54:12 +0530

    FloydWarshall algorithm for all pair shortest path for both -ve & +ve edge weights

--- _161FloydWarshallAlgo.java
+++ _161FloydWarshallAlgo.java
@@ -1,3 +1,25 @@
-public class _161FloydWarshallAlgo {
-    
+//GFG - Floyd Warshall
+//time comp - O(n^3)
+//space comp - O(1)
+
+class Solution
+{
+    public void shortest_distance(int[][] matrix)
+    {
+        int n = matrix.length;
+        for(int k = 0;k<n;k++){ //intermediate node
+            for(int i = 0;i<n;i++){ //source node
+                for(int j = 0;j<n;j++){ //destination node
+                    //check if path is available from i to j via k
+                    if(matrix[i][k] == -1 || matrix[k][j] == -1)
+                        continue; //skip this intermediate node
+                    int distViaK = matrix[i][k] + matrix[k][j];
+                    if(matrix[i][j] == -1) //if there is not direct path from i to j, then distViaK will be the new path from i to j via k
+                        matrix[i][j] = distViaK;
+                    else
+                        matrix[i][j] = Math.min(matrix[i][j], distViaK);
+                }
+            }
+        }
+    }
 }
